4af2758a1407c18376f69344edaea4a4d9831950
Shey Barpagga <sheybarpagga@gmail.com>
2022-05-28 14:27:19 -0700

    fixed login logic and checkout redirect

--- public/login.js
+++ public/login.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.json())
         .then(function(data) {
             data.forEach(element => {
+                console.log(element.email)
                 if(((element.email).trim().localeCompare(email)) == 0 && ((element.password).trim().localeCompare(password)) == 0) {
                     if((element.type).trim().localeCompare('admin') == 0) {
                         localStorage.setItem("logged-in", true);
