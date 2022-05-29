6fcc8f5409be55dd689049f0fd15be173bd7a7f6
Shey Barpagga <sheybarpagga@gmail.com>
2022-05-28 19:35:09 -0700

    got admin table working

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    getUser();
 })
 
 function getUser() {
@@ -10,7 +10,27 @@ function getUser() {
         var id = sessionStorage.getItem("user");
         var table = document.getElementById("user-table");
         for (const element of data) {
-            
+
+            tr = table.insertRow();
+
+            var id = tr.insertCell();
+            var email = tr.insertCell();
+            var type = tr.insertCell();
+            var update = tr.insertCell();
+
+            updateA = document.createElement("a");
+            updateA.setAttribute("id", `${element._id}`);
+            updateA.innerHTML = "Update";
+            updateA.classList.add("abutton");
+
+            updateA.addEventListener("click", () => {
+                window.location.href = "/update";
+            })
+
+            id.appendChild(document.createTextNode(`${element._id}`));
+            email.appendChild(document.createTextNode(`${element.email}`));
+            type.appendChild(document.createTextNode(`${element.type}`));
+            update.appendChild(updateA);
         }
     })
 }
